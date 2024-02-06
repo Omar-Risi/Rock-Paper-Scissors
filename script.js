3b9f4734c4c118bb2d71aadd9b4688bdf949f131
Omar Risi <orisi4business@gmail.com>
2024-02-06 15:02:43 +0400

    fixed bug in getComputerChoice, re-made switch statement into if else statements

--- script.js
+++ script.js
@@ -9,7 +9,8 @@
 function getComputerChoice() {
 
   const plays = ["rock", "paper","scissors"];
-  return plays[Math.floor(Math.random * plays.length)];
+  let randomIndex = plays[Math.floor(Math.random() * plays.length)];
+  return randomIndex;
 }
 
 function playRound() {
@@ -17,27 +18,25 @@ function playRound() {
   let computerChoice = getComputerChoice();
   let result = "";
 
-  switch(userChoice) {
-
-    case "rock": 
+  if (userChoice === "rock") {
       if (computerChoice === "rock") result = "Draw!";
       if (computerChoice === "paper") result = "you lost!, Paper beats rock";
       if (computerChoice === "scissors") result = "you won!, Rock Beats Scissors";
-      break;
-    case "paper": 
+  }
+  else if(userChoice === "paper") {
       if (computerChoice === "rock") result = "you won!, Paper beats rock";
       if (computerChoice === "paper") result = "Draw!";
       if (computerChoice === "scissors") result = "you lost, Scissors beat paper";
-      break;
-    case "scissors":
+  }
+  else if (userChoice === "scissors") {
       if (computerChoice === "rock") result = "you lost, Rock beats scissors";
       if (computerChoice === "paper") result = "you won, Scissors beat paper";
       if (computerChoice === "scissors") result = "draw";
-      break;
   }
 
   return result;
 }
+
 
 function playGame() {
   for(let i = 0; i < 5; i++) {
@@ -45,3 +44,7 @@ function playGame() {
     alert(result);
   }
 }
+
+//play game
+playGame();
+
