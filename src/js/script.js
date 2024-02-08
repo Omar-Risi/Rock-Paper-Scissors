c7be99d305bff95739a2b2c22544cac0b149962b
Omar Risi <orisi4business@gmail.com>
2024-02-08 20:43:51 +0400

    game logic added

--- src/js/script.js
+++ src/js/script.js
@@ -1,39 +1,64 @@
 /*
-* 
-*
-*         This Project is About getting input from user (rock, paper, scissors) and matching it against 
-*         computer choice. 
-*
-* */
+ *
+ *
+ *         This Project is About getting input from user (rock, paper, scissors) and matching it against
+ *         computer choice.
+ *
+ * */
 
+// UI elements 
+const gameText = document.querySelector(".game-result");
+const rockButton = document.querySelector("#rock-button");
+const paperButton = document.querySelector("#paper-button");
+const scissorsButton = document.querySelector("#scissors-button");
+
+// randomly pick a choice for the computer
 function getComputerChoice() {
-
-  const plays = ["rock", "paper","scissors"];
+  const plays = ["rock", "paper", "scissors"];
   let randomIndex = plays[Math.floor(Math.random() * plays.length)];
   return randomIndex;
 }
 
-function playRound(choice) {
-  let userChoice = choice;
+// play per round of rps
+function playRound(userChoice) {
   let computerChoice = getComputerChoice();
   let result = "";
 
   if (userChoice === "rock") {
-      if (computerChoice === "rock") result = "Draw!";
-      if (computerChoice === "paper") result = "you lost!, Paper beats rock";
-      if (computerChoice === "scissors") result = "you won!, Rock Beats Scissors";
+    if (computerChoice === "rock") result = "Draw!";
+    if (computerChoice === "paper") result = "you lost!";
+    if (computerChoice === "scissors") result = "you won!";
+  } else if (userChoice === "paper") {
+    if (computerChoice === "rock") result = "you won!";
+    if (computerChoice === "paper") result = "Draw!";
+    if (computerChoice === "scissors") result = "you lost!";
+  } else if (userChoice === "scissors") {
+    if (computerChoice === "rock") result = "you lost!";
+    if (computerChoice === "paper") result = "you won!";
+    if (computerChoice === "scissors") result = "draw!";
   }
-  else if(userChoice === "paper") {
-      if (computerChoice === "rock") result = "you won!, Paper beats rock";
-      if (computerChoice === "paper") result = "Draw!";
-      if (computerChoice === "scissors") result = "you lost, Scissors beat paper";
-  }
-  else if (userChoice === "scissors") {
-      if (computerChoice === "rock") result = "you lost, Rock beats scissors";
-      if (computerChoice === "paper") result = "you won, Scissors beat paper";
-      if (computerChoice === "scissors") result = "draw";
-  }
+  
+  //change game result 
+
+  gameText.textContent = result.toUpperCase();
+
+  if (result === "you won!")
+    gameText.style.color = "#0f0";
+  else if (result === "you lost!")
+    gameText.style.color = "#f00";
+  else 
+    gameText.style.color = "#fff";
 
   return result;
 }
+
+// set choice per button using property .choice
+rockButton.choice = "rock";
+paperButton.choice = "paper";
+scissorsButton.choice = "scissors";
+
+// add event listener to play round
+rockButton.onclick = () => playRound(rockButton.choice);
+paperButton.onclick = () => playRound(paperButton.choice);
+scissorsButton.onclick = () => playRound(scissorsButton.choice);
 
